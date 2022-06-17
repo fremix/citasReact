dcf41c685af58404b1e08e5ef71eac6b1a0c7e74
fremix <microsys.net@gmail.com>
2022-06-17 02:01:47 -0500

    update

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,13 +3,24 @@ const Formulario = () => {
     <div className="md:w-1/2 lg:w-2/5">  
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-        <p className="text-lg mt-5 text-center">
+        <p className="text-lg mt-5 text-center mb-10">
             Añade Pacientes y {''}
             <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
 
-        <form action="">
-          
+        <form className="bg-white shadow-md rounded-lg py-10 px-5">
+            <div>
+              <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+                Nombre Mascota
+              </label> {''}
+
+            <input
+            id="mascota" 
+              type="text"
+              placeholder="Nombre de la Mascota" 
+              className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
+            />
+            </div>
         </form>
     </div>
   )
